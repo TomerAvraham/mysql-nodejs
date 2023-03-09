f3b07aaa4d223ea2ffb65058afdd4649bbbb5c2a
Tomer Avraham <avrahamtomer710@gmail.com>
2023-03-09 18:10:50 +0200

    start the army

--- server/src/server.js
+++ server/src/server.js
@@ -4,17 +4,7 @@ const config = require("./config");
 const bodyParser = require("body-parser");
 const app = express();
 
-const categoriesRoutes = require("./routes/categoriesRoutes");
-
-function test(one, ...values) {
-  console.log(values);
-}
-
-test(1, 2, 3, "adi", 5);
-
 app.use(bodyParser.json());
-
-app.use("/", categoriesRoutes);
 
 const port = config.port;
 app.listen(port, () => console.log("Running on " + port));
